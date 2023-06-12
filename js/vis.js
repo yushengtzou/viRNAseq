5b8e25dac5ff632b02028c48c28c8436d3d42dc6
Yu Sheng Tzou <daniel.tzou1021@gmail.com>
2023-06-12 16:16:49 +0800

    Updated 20230613

--- js/vis.js
+++ js/vis.js
@@ -1,36 +1,73 @@
-function createScatterPlot(elementId, data, xValue, yValue) {
-    const margin = { top: 25, right: 20, bottom: 35, left: 40 };
-    const width = (960 - margin.left - margin.right)*3/4;
-    const height = (500 - margin.top - margin.bottom)*3/4;
+function penguinPlot(elementId, data, xValue, yValue) {
+        const margin = { top: 25, right: 20, bottom: 35, left: 40 };
+        const width = (960 - margin.left - margin.right)*3/4;
+        const height = (500 - margin.top - margin.bottom)*3/4;
 
-    const x = d3.scaleLinear().range([margin.left, width - margin.right]);
-    const y = d3.scaleLinear().range([height - margin.bottom, margin.top]);
+        const x = d3.scaleLinear().range([margin.left, width - margin.right]);
+        const y = d3.scaleLinear().range([height - margin.bottom, margin.top]);
 
-    const xAxis = g => g
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x));
-    const yAxis = g => g
-    .attr("transform", `translate(${margin.left},0)`)
-    .call(d3.axisLeft(y));
+        const xAxis = g => g
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(x));
+        const yAxis = g => g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y));
 
-    x.domain(d3.extent(data, d => +d[xValue])).nice();
-    y.domain(d3.extent(data, d => +d[yValue])).nice();
+        x.domain(d3.extent(data, d => +d[xValue])).nice();
+        y.domain(d3.extent(data, d => +d[yValue])).nice();
 
-    const svg = d3.select(`#${elementId}`)
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom);
+        const svg = d3.select(`#${elementId}`)
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom);
 
-    svg.append('g').call(xAxis);
-    svg.append('g').call(yAxis);
+        svg.append('g').call(xAxis);
+        svg.append('g').call(yAxis);
 
-    svg.append('g')
-    .selectAll('circle')
-    .data(data)
-    .join('circle')
-    .filter(d => !isNaN(+d[xValue]) && !isNaN(+d[yValue]))  // Skip invalid data objects.
-    .attr('cx', d => x(+d[xValue]))
-    .attr('cy', d => y(+d[yValue]))
-    .attr('r', 3.5);
+        svg.append('g')
+        .selectAll('circle')
+        .data(data)
+        .join('circle')
+        .filter(d => !isNaN(+d[xValue]) && !isNaN(+d[yValue]))  // Skip invalid data objects.
+        .attr('cx', d => x(+d[xValue]))
+        .attr('cy', d => y(+d[yValue]))
+        .attr('r', 3.5);
+}
+
+function tsnePlot(elementId, data, xValue, yValue) {
+        console.log(data);
+        const margin = { top: 25, right: 20, bottom: 35, left: 40 };
+        const width = (960 - margin.left - margin.right)*3/4;
+        const height = (500 - margin.top - margin.bottom)*3/4;
+
+        const x = d3.scaleLinear().range([margin.left, width - margin.right]);
+        const y = d3.scaleLinear().range([height - margin.bottom, margin.top]);
+
+        const xAxis = g => g
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(x));
+        const yAxis = g => g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y));
+
+        x.domain(d3.extent(data, d => +d[xValue])).nice();
+        y.domain(d3.extent(data, d => +d[yValue])).nice();
+
+        const svg = d3.select(`#${elementId}`)
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom);
+
+        svg.append('g').call(xAxis);
+        svg.append('g').call(yAxis);
+
+        svg.append('g')
+        .selectAll('circle')
+        .data(data)
+        .join('circle')
+        // Skip invalid data objects.
+        .filter(d => !isNaN(+d[xValue]) && !isNaN(+d[yValue]))  
+        .attr('cx', d => x(+d[xValue]))
+        .attr('cy', d => y(+d[yValue]))
+        .attr('r', 3.5);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -41,13 +78,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
         let reader = new FileReader();
         reader.onload = function(e) {
-            // document.getElementById('gettingStartedCard').style.display = 'none';
             document.getElementById('svg').style.display = 'visible';
             let contents = e.target.result;
             let data = d3.csvParse(contents);
 
-            createScatterPlot("scatterplot", data, 'flipper_length_mm', 'body_mass_g');
-            createScatterPlot("scatterplot2", data, 'culmen_length_mm', 'body_mass_g');
+            tsnePlot("scatterplot", data, 'TSNE-1', 'TSNE-2');
+            // penguinPlot("scatterplot", data, 'flipper_length_mm', 'body_mass_g');
+            // createScatterPlot("scatterplot2", data, 'culmen_length_mm', 'body_mass_g');
         };
         var introCard = document.getElementById('intro_card');
         document.getElementsByTagName('svg')[0].style.visibility = 'visible';
@@ -56,3 +93,29 @@ document.addEventListener("DOMContentLoaded", function() {
         reader.readAsText(file);
     });
 });
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById('submit').addEventListener('click', function(e) {
+//     e.preventDefault();  // Prevent form from submitting
+//     let fileInput = document.getElementById('csvFile');
+//     let file = fileInput.files[0];
+//
+//     let reader = new FileReader();
+//     reader.onload = function(e) {
+//         let contents = e.target.result;
+//         let data = d3.csvParse(contents);
+//         console.log(data);
+//
+//         // tsnePlot("scatterplot", data, 'TSNE-1', 'TSNE-2');
+//         penguinPlot("scatterplot", data, 'flipper_length_mm', 'body_mass_g');
+//         // createScatterPlot("scatterplot2", data, 'culmen_length_mm', 'body_mass_g');
+//
+//         document.getElementById('svg').style.visibility = 'display'; // corrected from display to visibility
+//         var introCard = document.getElementById('intro_card');
+//         introCard.style.display = 'none';
+//     };
+//
+//     reader.readAsText(file);
+//     });
+// });
+//
--- php/upload_file.php
+++ php/upload_file.php
@@ -13,8 +13,6 @@ if(isset($_POST["submit"])) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
         
-        // Removed the database related code here...
-
         //Redirect to the index page after successful file upload
         header("Location: ../index.php"); // This line will redirect you to index.php
         exit; // Ensure no further output is sent
@@ -24,61 +22,4 @@ if(isset($_POST["submit"])) {
 }
 ?>
 
-// <?php
-//
-// echo 'Login script accessed';
-//
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
-//
-// // Your database config might be different
-// $servername = "localhost";
-// $username = "root";   // default username for XAMPP is root
-// $password = "";       // default password for XAMPP is blank
-// $dbname = "viRNAseq"; // change this to your database name
-//
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-//
-// // Check connection
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
-//
-// if(isset($_POST["submit"])) {
-//     $target_dir = "../uploads/";
-//     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-//
-//     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-//         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
-//         
-//         $file = fopen($target_file,"r");
-//
-//         $header = fgetcsv($file); // Get the header row
-//         while (($data = fgetcsv($file)) !== FALSE) {
-//             $species = mysqli_real_escape_string($conn, $data[0]);
-//             $island = mysqli_real_escape_string($conn, $data[1]);
-//             $culmen_length_mm = is_numeric($data[2]) ? mysqli_real_escape_string($conn, $data[2]) : 'NULL';
-//             $culmen_depth_mm = is_numeric($data[3]) ? mysqli_real_escape_string($conn, $data[3]) : 'NULL';
-//             $flipper_length_mm = is_numeric($data[4]) ? mysqli_real_escape_string($conn, $data[4]) : 'NULL';
-//             $body_mass_g = is_numeric($data[5]) ? mysqli_real_escape_string($conn, $data[5]) : 'NULL';
-//             $sex = mysqli_real_escape_string($conn, $data[6]);
-//
-//             $query = "INSERT INTO species_data(species, island, culmen_length_mm, culmen_depth_mm, flipper_length_mm, body_mass_g, sex)
-//             VALUES ('$species', '$island', $culmen_length_mm, $culmen_depth_mm, $flipper_length_mm, $body_mass_g, '$sex')";
-//
-//             if (mysqli_query($conn, $query)) {
-//                 // echo 'Row inserted!<br>';
-//             } else {
-//                 echo 'Error inserting row: ' . mysqli_error($conn) . '<br>';
-//             }
-//         }
-//         fclose($file);
-//         header("Location: ../index.php"); // This line will redirect you to index.php
-//         exit; // Ensure no further output is sent
-//     } else {
-//         echo "Sorry, there was an error uploading your file.";
-//     }
-// }
-// ?>
+<?php
