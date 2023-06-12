19956c4963177b5d3181c3ae386e1a66d54c7f1d
Yu Sheng Tzou <daniel.tzou1021@gmail.com>
2023-06-13 00:51:36 +0800

    Updated 20230613

--- js/vis.js
+++ js/vis.js
@@ -1,5 +1,4 @@
 function tsnePlot(elementId, data, barcode, xValue, yValue, cluster) {
-    console.log(data);
     const margin = { top: 56, right: 29, bottom: 2, left: 29 };
     const size = 600; 
     const width = size - margin.left - margin.right;
@@ -17,8 +16,9 @@ function tsnePlot(elementId, data, barcode, xValue, yValue, cluster) {
 
     // Color scale
     const color = d3.scaleOrdinal()
-    .domain(d3.range(1, 8))  // Domain 1-7 for your clusters.
-    .range(d3.schemeSet3.slice(0, 7));  // 7 colors from schemeSet3.
+    // Domain based on the number of clusters
+    .domain(d3.range(1, d3.max(data, d => +d[cluster]) + 1)) 
+    .range(d3.schemeSet3);  // 12-color scheme
 
     x.domain(d3.extent(data, d => +d[xValue])).nice();
     y.domain(d3.extent(data, d => +d[yValue])).nice();
@@ -29,7 +29,7 @@ function tsnePlot(elementId, data, barcode, xValue, yValue, cluster) {
 
     svg.append("text")
     .attr("x", (width + margin.left + margin.right) / 2+10)
-    .attr("y", margin.top / 2)
+    .attr("y", margin.top / 3)
     .attr("text-anchor", "middle")
     .style("font-size", "20px") 
     .text("t-SNE plot"); 
@@ -41,59 +41,13 @@ function tsnePlot(elementId, data, barcode, xValue, yValue, cluster) {
     .selectAll('circle')
     .data(data)
     .join('circle')
-    // Skip invalid data objects.
     .filter(d => !isNaN(+d[xValue]) && !isNaN(+d[yValue]))  
     .attr('cx', d => x(+d[xValue]))
     .attr('cy', d => y(+d[yValue]))
     .attr('r', 3.5)
-    .attr('fill', d => color(d[cluster]));  // Set fill color based on cluster.
+    // Set fill color based on cluster.
+    .attr('fill', d => color(d[cluster]));  
 }
-
-//
-// function tsnePlot(elementId, data, barcode, xValue, yValue, cluster) {
-//     console.log(data);
-//     const margin = { top: 56, right: 29, bottom: 2, left: 29 };
-//     const size = 480; 
-//     const width = size - margin.left - margin.right;
-//     const height = size - margin.top - margin.bottom;
-//
-//     const x = d3.scaleLinear().range([margin.left, width - margin.right]);
-//     const y = d3.scaleLinear().range([height - margin.bottom, margin.top]);
-//
-//     const xAxis = g => g
-//     .attr("transform", `translate(0,${height - margin.bottom})`)
-//     .call(d3.axisBottom(x));
-//     const yAxis = g => g
-//     .attr("transform", `translate(${margin.left},0)`)
-//     .call(d3.axisLeft(y));
-//
-//     x.domain(d3.extent(data, d => +d[xValue])).nice();
-//     y.domain(d3.extent(data, d => +d[yValue])).nice();
-//
-//     const svg = d3.select(`#${elementId}`)
-//     .attr('width', width + margin.left + margin.right)
-//     .attr('height', height + margin.top + margin.bottom);
-//
-//     svg.append("text")
-//     .attr("x", (width + margin.left + margin.right) / 2+50)
-//     .attr("y", margin.top / 3)
-//     .attr("text-anchor", "middle")
-//     .style("font-size", "20px") 
-//     .text("t-SNE plot"); 
-//
-//     svg.append('g').call(xAxis);
-//     svg.append('g').call(yAxis);
-//
-//     svg.append('g')
-//     .selectAll('circle')
-//     .data(data)
-//     .join('circle')
-//     // Skip invalid data objects.
-//     .filter(d => !isNaN(+d[xValue]) && !isNaN(+d[yValue]))  
-//     .attr('cx', d => x(+d[xValue]))
-//     .attr('cy', d => y(+d[yValue]))
-//     .attr('r', 3.5);
-// }
 
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('submit').addEventListener('click', function(e) {
